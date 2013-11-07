39aafe380cb1ad1b88a3eed5fec78782e6166432
Stephen Sowole <tunsow@hotmail.com>
2013-11-07 09:31:37 +0000

    Game file commit
    - created xcode file for fruit machine game

---
+++ Fruit Machine Game/Fruit Machine Game/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Fruit Machine Game
+//
+//  Created by Stephen Sowole on 07/11/2013.
+//  Copyright G52GRP 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
