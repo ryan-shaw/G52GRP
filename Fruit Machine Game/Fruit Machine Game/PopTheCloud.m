e059e1faabf6eae653518571915d22d799a2a0c6
Matthew Herod <mxh42u@england.cs.nott.ac.uk>
2014-03-24 12:13:22 +0000

    pop de cloud

--- Fruit Machine Game/Fruit Machine Game/PopTheCloud.m
+++ Fruit Machine Game/Fruit Machine Game/PopTheCloud.m
@@ -8,7 +8,6 @@
 
 #import "PopTheCloud.h"
 
-
 @implementation PopTheCloud
 
 + (CCScene *) scene {
@@ -29,6 +28,9 @@
     
 	if( (self=[super init])) {
         
+        popped = 0;
+        score = 0;
+       
         CGSize window = [[CCDirector sharedDirector] winSize];
         
         CCSprite *background;
@@ -37,27 +39,68 @@
         
         [self addChild:background z:-1];
         
+        clouds = [[NSMutableArray alloc] init];
+       
+        // Add Sprites to Array.
+        
+        // clouds
         CCSprite *cloud1 = [CCSprite spriteWithFile:@"cloud.png"];
         CCSprite *cloud2 = [CCSprite spriteWithFile:@"cloud.png"];
         CCSprite *cloud3 = [CCSprite spriteWithFile:@"cloud.png"];
         
-        cloud1.anchorPoint = ccp(0,0);
-        cloud2.anchorPoint = ccp(0,0);
-        cloud3.anchorPoint = ccp(0,0);
+        [clouds addObject:cloud1];
+        [clouds addObject:cloud2];
+        [clouds addObject:cloud3];
         
-        cloud1.position = ccp(window.height / 3 , 10);
-        cloud1.position = ccp((window.height / 3) * 2, 15);
-        cloud1.position = ccp(window.height, 20);
+        // middle x, y
+        int y = window.height / 2;
+        int x = window.width / 2;
+        
+        // position them all spaced out evenly
+        cloud1.position = ccp(x, y - (y / 1.5));
+        cloud2.position = ccp(x, y);
+        cloud3.position = ccp(x, y + (y / 1.5));
         
         [self addChild:cloud1 z:1];
         [self addChild:cloud2 z:1];
         [self addChild:cloud3 z:1];
         
+        scoreLabel = [CCLabelTTF labelWithString:@"Score: 0" fontName:@"Verdana" fontSize:25.0];
+        scoreLabel.anchorPoint = ccp(0, 0);
+        scoreLabel.color = ccc3(255,0,0);
+        scoreLabel.position = ccp(window.width/4, window.height/2);
+        
+        [self addChild:scoreLabel z:10];
         
     }
     
     self.touchEnabled = YES;
     return self;
+}
+
+- (void) score {
+        score += 10;
+        [scoreLabel setString:[NSString stringWithFormat:@"Score: %d", score]];
+}
+
+- (void) update {
+    
+   
+}
+
+- (void) ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event {
+    
+    UITouch *touch = [touches anyObject];
+    CGPoint firstTouch = [touch locationInView:[touch view]];
+    firstTouch = [[CCDirector sharedDirector] convertToGL:firstTouch];
+    
+    for (CCSprite *cloud in clouds) {
+        if (CGRectContainsPoint(cloud.boundingBox, firstTouch)) {
+            [self score];
+        }
+    }
+    
+    
 }
 
 @end
